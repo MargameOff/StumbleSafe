0a65171a1ff68574604b9db6d1005854c7d0b642
MargameOff <margames.tutorial@gmail.com>
2024-02-22 12:26:20 +0100

    Add security

--- Back/server.js
+++ Back/server.js
@@ -1,13 +1,32 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import userRoutes from './routes/users.routes.js';
+import path from 'path';
+import dotenv from 'dotenv';
 
-const APP = express()
-
+const APP = express();
 const PORT = 8080;
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const envPath = path.resolve(__dirname, '../.env');
 
-APP.listen(PORT, () => {
-    console.log(`Stumble app listening on port ${PORT}`)
-})
+//Set up the .env file
+dotenv.config({ path: envPath });
+
+mongoose.connect("mongodb://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@" + process.env.MONGODB_HOST + ":" + process.env.MONGODB_LOCAL_PORT + "/" + process.env.MONGODB_DATABASE + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, authSource: 'admin' })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start the server
+        APP.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+//Middleware
+APP.use(express.json());
+
 
 //Routes
 APP.use('/api/users', userRoutes);
@@ -23,3 +42,4 @@ APP.get('/status', (req, res) => {
 APP.get('*', (req, res) => {
     res.status(404).send('404 Not Found, go check the documentation for the available routes.');
 })
+
