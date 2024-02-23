9c33ec335552582bd0c1abe83d63476524addad2
Chloé B <chloe.buttigieg12@gmail.com>
2024-02-23 19:13:23 +0100

    Patch dotenv load error

--- Back/tools/database.js
+++ Back/tools/database.js
@@ -3,11 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { log } from 'console';
 
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-const envPath = path.resolve(__dirname, '../../.env');
-dotenv.config({ path: envPath});
+dotenv.config({ path: '../.env'});
 if (!process.env.MONGODB_USER || !process.env.MONGODB_PASSWORD || !process.env.MONGODB_HOST || !process.env.MONGODB_LOCAL_PORT || !process.env.MONGODB_DATABASE) {
     log('Le fichier .env est mal configuré ou manquant');
     process.exit(1);
