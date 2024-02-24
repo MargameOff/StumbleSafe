b124d0b4d2eefd768dffaff9435f6e002ae9734c
enorart <enora.sicre@insa-lyon.fr>
2024-02-25 00:08:04 +0100

    maj : ajout du nom d'utilisateur unique pour les utilisateurs

--- Back/models/user.models.js
+++ Back/models/user.models.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     nom: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     nom_affiche:
         {
