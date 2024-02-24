82c003a3b56fb045ffabb737ca09f54f214147e6
enorart <enora.sicre@insa-lyon.fr>
2024-02-24 23:50:31 +0100

    ajout du nom d'utilisateur unique pour les utilisateurs

--- Back/controllers/users/signup.controllers.js
+++ Back/controllers/users/signup.controllers.js
@@ -9,21 +9,32 @@ const signup = async (req, res) => {
         // Récupérer les données de l'utilisateur à partir du corps de la requête
         console.log(req.body.userInfos);
         // Vérifier si l'utilisateur existe déjà dans la base de données
-        const { nom, email, password } = req.body.userInfos; // Assurez-vous que req.body.userInfos contient les données correctes
-        const userExists = await checkIfUserExists(email);
-        if (userExists) {
+        const { nom, nom_affiche, email, password } = req.body.userInfos; // Assurez-vous que req.body.userInfos contient les données correctes
+        const userEmailExists = await checkIfUserEmailExists(email);
+        const userNameExists = await checkIfUserNameExists(nom);
+
+        // Vérification de l'email (unique)
+        if (userEmailExists) {
             return res.status(400).json({ message: 'Un utilisateur avec cet email existe déjà' });
         } else if (!checkIfIsValidMail(email)) {
             return res.status(400).json({ message: 'L\'adresse email n\'est pas valide' });
         }
+
+        // Vérification de l'username (unique)
+        if (userNameExists){
+            return res.status(400).json({message : 'Ce nom d\'utilisateur est déjà utilisé'});
+        }
         if(!req.body.userInfos){
             return res.status(400).json({ message: 'Les informations de l\' utilisateur sont manquantes' });
         }
-        if(!nom || !email || !password){
+
+        // Vérification des champs obligatoires
+        if(!nom || !email || !password || !nom_affiche){
             return res.status(400).json({ message: 'Les informations de l\' utilisateur sont manquantes' });
         }
+
         // Créer un nouvel utilisateur en utilisant le modèle Mongoose
-        const newUser = new UserModel({ nom, email, password });
+        const newUser = new UserModel({ nom, nom_affiche, email, password });
         
         // Enregistrer l'utilisateur dans la base de données
         const user = await newUser.save();
@@ -39,9 +50,18 @@ const signup = async (req, res) => {
     }
 }
 
-const checkIfUserExists = async(email) => {
-    // Vérifier si l'utilisateur existe déjà dans la base de données
+const checkIfUserEmailExists = async(email) => {
+    // Vérifier si l'utilisateur avec cet email existe déjà dans la base de données
     const user = await UserModel.findOne({ email });
+    if (user) {
+        return true;
+    }
+    return false;
+}
+
+const checkIfUserNameExists = async(nom) => {
+    // Vérifier si l'utilisateur avec ce nom d'utilisateur existe déjà dans la base de données
+    const user = await UserModel.findOne({ nom });
     if (user) {
         return true;
     }
--- Back/models/user.models.js
+++ Back/models/user.models.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    nom_affiche:
+        {
+            type: String,
+            required: true
+        },
     email: {
         type: String,
         required: true,
