e04b98016d80f8c4ca6df57e7a4e0e01e4e3afd1
Mathis² <mathisfr45@gmail.com>
2024-03-08 13:19:20 +0100

    Added group screen in new interface

--- Front/navigation/BottomBar.js
+++ Front/navigation/BottomBar.js
@@ -21,7 +21,7 @@ export default function BottomBar() {
                 <View style={styles.subButton}/>
                 <Ionicons name="add-circle" style={styles.button} size={90} color="#50E3A5" />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+            <TouchableOpacity onPress={() => navigation.navigate("group/creating")}>
                 <Ionicons name="people" size={32} color="white" />
             </TouchableOpacity>
         </LinearGradient>
